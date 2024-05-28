83b7481c355dbc5bfc4e2bc7c115499eea5aa080
NguyenNgan03 <simple.270123@gmail.com>
2024-05-28 08:14:58 +0700

    UPDATE/UI/BOOKING

--- src/components/page/Booking/Index.jsx
+++ src/components/page/Booking/Index.jsx
@@ -79,47 +79,6 @@ export function Content() {
       <h2 className="m-3 title">MANAGE BOOKING</h2>
       <div className="tableBooking mt-3">
       <DataTable columns={columns} data={data} pagination />
-
-        {/* <table className="mx-3 table table-striped">
-          <thead>
-            <tr>
-              <th scope="col">ID</th>
-              <th scope="col">User Name</th>
-              <th scope="col">Phone</th>
-              <th scope="col">Room Number</th>
-              <th scope="col">Room Price</th>
-              <th scope="col">Check in date</th>
-              <th scope="col">Check out date</th>
-              <th scope="col">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {bookings.map((booking) => (
-              <tr key={booking.id}>
-                <td>{booking.id}</td>
-                <td>{booking.user.name}</td>
-                <td>{booking.user.phone}</td>
-                <td>{booking.room.number}</td>
-                <td>{booking.room.price}</td>
-                <td>{booking.check_in_date}</td>
-                <td>{booking.check_out_date}</td>
-                <td>
-                  <div className="d-flex">
-                    <Link
-                      to={`/bookings/edit/${booking.id}`}
-                      className="mx-3 btn btn-warning btn-sm"
-                    >
-                      Edit
-                    </Link>
-                    <Link to="/delete" className="btn btn-danger btn-sm">
-                      Delete
-                    </Link>
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table> */}
       </div>
     </div>
   );
