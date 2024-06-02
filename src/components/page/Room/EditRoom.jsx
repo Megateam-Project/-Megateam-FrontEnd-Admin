580670f8f0d6ac923a6c96e19455feeb196ba5a3
NguyenNgan03 <simple.270123@gmail.com>
2024-06-02 22:21:09 +0700

    UI/User/Room

--- src/components/page/Room/EditRoom.jsx
+++ src/components/page/Room/EditRoom.jsx
@@ -1,101 +1,211 @@
-import React, { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
-import { ArrowLeftOutlined } from "@ant-design/icons";
-import { DatePicker, Form, Input, Select, Button } from "antd";
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import baseApi from "../../../shared/services/base.api";
-import moment from "moment";
+import { BASE_URL } from "../../../shared/constants/constants.js";
+
 export function EditRoom() {
-  const { Option } = Select;
-  const { bookingId } = useParams();
-  const [bookings, setBookings] = useState([]);
+  const { id } = useParams();
+  const [formData, setFormData] = useState({
+    name: "",
+    type: "",
+    description: "",
+    price: "",
+    image: null,
+    convenient: "",
+    number: "",
+    discount: "",
+    create_by: "admin",
+  });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await baseApi.getDetailApi(`bookings/${bookingId}`);
-      setBookings(response);
+    const fetchRoom = async () => {
+      setLoading(true);
+      try {
+        const response = await baseApi.getApi(BASE_URL + `rooms/${id}`);
+        setFormData({
+          ...response,
+          image: null, // Đặt hình ảnh thành null để người dùng có thể cập nhật ảnh mới
+        });
+      } catch (err) {
+        setError("Failed to fetch room data. Please try again later.");
+      } finally {
+        setLoading(false);
+      }
     };
-    fetchData();
-  }, [bookingId]);
-  const bookingArr = Object.values(bookings);
+    fetchRoom();
+  }, [id]);
+
+  const handleFormDataChange = (e) => {
+    const { name, value, files } = e.target;
+    if (name === "image") {
+      setFormData((prevData) => ({
+        ...prevData,
+        [name]: files[0],
+      }));
+    } else {
+      setFormData((prevData) => ({
+        ...prevData,
+        [name]: value,
+      }));
+    }
+  };
+
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError(null);
+    try {
+      const formDataToSend = new FormData();
+      for (const key in formData) {
+        formDataToSend.append(key, formData[key]);
+      }
+      await baseApi.putApi(BASE_URL + `rooms/${id}`, formDataToSend);
+      setLoading(false);
+      navigate("/rooms");
+    } catch (error) {
+      console.error("Error updating room:", error);
+      setError("Failed to update room. Please try again later.");
+      setLoading(false);
+    }
+  };
+
   return (
     <div>
-      <h2 className="mt-3 text-center title">MANAGE BOOKING</h2>
+      <h2 className="mt-3 text-center title">Edit Room</h2>
       <div className="buttonBack m-3">
-        <Link to="/rooms" className="btn btn-secondary mx-3">
-          Back <ArrowLeftOutlined />
-        </Link>
+        <button
+          onClick={() => navigate("/rooms")}
+          className="btn btn-secondary mx-3"
+        >
+          Back
+        </button>
       </div>
       <div className="d-flex flex-column justify-content-between align-items-center">
-        <form className="rounded border border-danger-subtle w-50 p-3">
-          <h3 className="text-center mt-2 mb-4">Edit Booking</h3>
-          <div>
-            {bookingArr.map((booking) => (
-              <React.Fragment key={booking.id}>
-                <Form.Item
-                  label="User name"
-                  labelCol={{ span: 4 }}
-                  wrapperCol={{ span: 20 }}
-                >
-                  <Input defaultValue={booking.user.name} />
-                </Form.Item>
-                <Form.Item
-                  label="Phone number"
-                  labelCol={{ span: 4 }}
-                  wrapperCol={{ span: 20 }}
-                >
-                  <Input defaultValue={booking.user.phone} />
-                </Form.Item>
-                <Form.Item
-                  label="Room number"
-                  labelCol={{ span: 4 }}
-                  wrapperCol={{ span: 20 }}
-                >
-                  <Input defaultValue={booking.room.number} />
-                </Form.Item>
-                <Form.Item
-                  name="check_in_date"
-                  label="Check in date"
-                  labelCol={{ span: 4 }}
-                  wrapperCol={{ span: 20 }}
-                >
-                  <DatePicker
-                    showTime
-                    format="YYYY-MM-DD HH:mm:ss"
-                    defaultValue={moment(booking.check_in_date)}
-                  />
-                </Form.Item>
-                <Form.Item
-                  name="check_out_date"
-                  label="Check out date"
-                  labelCol={{ span: 4 }}
-                  wrapperCol={{ span: 20 }}
-                >
-                  <DatePicker
-                    showTime
-                    format="YYYY-MM-DD HH:mm:ss"
-                    defaultValue={moment(booking.check_out_date)}
-                  />
-                </Form.Item>
-                <Form.Item
-                  name="update_by"
-                  label="Update By"
-                  labelCol={{ span: 4 }}
-                  wrapperCol={{ span: 20 }}
-                >
-                  <Select placeholder="select">
-                    <Option value="admin">Admin</Option>
-                    <Option value="user">User</Option>
-                  </Select>
-                </Form.Item>
-              </React.Fragment>
-            ))}
+        {error && (
+          <div className="alert alert-danger" role="alert">
+            {error}
           </div>
-          <Form.Item wrapperCol={{ offset: 10, span: 10 }}>
-            <Link to="/bookings">
-              <Button type="primary" htmlType="submit">
-                Update
-              </Button>
-            </Link>
-          </Form.Item>
+        )}
+        <form
+          className="rounded border border-danger-subtle w-50 p-3"
+          onSubmit={handleUpdate}
+        >
+          <h3 className="text-center mt-2 mb-4">Room Details</h3>
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">
+              Room Name
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleFormDataChange}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="type" className="form-label">
+              Room Type
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="type"
+              name="type"
+              value={formData.type}
+              onChange={handleFormDataChange}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="description" className="form-label">
+              Description
+            </label>
+            <textarea
+              className="form-control"
+              id="description"
+              name="description"
+              value={formData.description}
+              onChange={handleFormDataChange}
+              required
+            ></textarea>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="price" className="form-label">
+              Price
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="price"
+              name="price"
+              value={formData.price}
+              onChange={handleFormDataChange}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="image" className="form-label">
+              Image
+            </label>
+            <input
+              type="file"
+              className="form-control"
+              id="image"
+              name="image"
+              onChange={handleFormDataChange}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="convenient" className="form-label">
+              Convenient
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="convenient"
+              name="convenient"
+              value={formData.convenient}
+              onChange={handleFormDataChange}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="number" className="form-label">
+              Number
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="number"
+              name="number"
+              value={formData.number}
+              onChange={handleFormDataChange}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="discount" className="form-label">
+              Discount
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="discount"
+              name="discount"
+              value={formData.discount}
+              onChange={handleFormDataChange}
+              required
+            />
+          </div>
+          <button type="submit" className="btn btn-primary" disabled={loading}>
+            {loading ? "Updating..." : "Update Room"}
+          </button>
         </form>
       </div>
     </div>
